bf0cb844501c0214fae1d0d93f364fd42a7cf83c
Troian Yevhenii <vitalich.ua@gmail.com>
2022-06-28 21:41:28 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import { Container } from "utils/Container.styled";
+import data from '../components/Statistics/data.json';
+import friends from './FriendList/friends.json'
+import transactions from './TransactionHistory/transactions.json'
+import { Box } from "../utils/Box";
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+ <Container>
+        <Profile/>
+      <Box display ="flex">
+        <Statistics title="Upload stats" items={data} />
+        </Box>
+      <FriendList items={friends} />
+      <TransactionHistory items={transactions} />
+      </Container>
   );
 };
